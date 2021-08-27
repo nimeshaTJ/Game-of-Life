7042b590ac1f005bb3a8f4fc063f3a0904fe27e9
nimeshaTJ <nimeshatj@gmail.com>
2021-08-27 12:40:07 +0530

    Added ability to clear screen with c, fill screen with f, and click+drag the mouse to add/remove multiple cells fluidly. Also updated so that the grid lies on a toroid and wraps around edges. Added ability to place a preset pattern onto the board from the command line. Presets are stored in patterns.py.

--- GameofLife.py
+++ GameofLife.py
@@ -1,39 +1,41 @@
 import numpy as np
 import pygame
+import patterns
+import sys
 
-#Function that fills in cells on the board given a list of coordinates for each cell
-def fill_cells(points):
+#Function that fills in patterns on the board given a list of coordinates and an origin point
+def fill_cells(origin,size,points):
+	subgrid = current_state[origin[0]:origin[0]+size[0],origin[1]:origin[1]+size[1]]
 	for point in points:
-		initial_state[point[0]+1,point[1]+1] = 1
+		subgrid[point[0],point[1]] = 1
 
 #Function that calculates and returns the state of all cells at the next timestep
 def update():
 	#Temporary variable to store the state of all cells at the next timestep
 	next_state = current_state.copy()
-	#Iterating through the array of cells, ignoring the outer edges
-	for rowindex, row in enumerate(next_state[1:-1]):
+	#Iterating through the array of cells
+	for rowindex, row in enumerate(next_state):
 
-		for colindex, value in enumerate(row[1:-1]):
-			#Creating a subgrid of the surrounding 8 cells for each cell 
-			subgrid = current_state[rowindex:rowindex+3,colindex:colindex+3].copy()
-			subgrid[1,1] = 0
-			#Counting the number of living cells around each cell
-			live_cells = np.count_nonzero(subgrid==1)
-			
+		for colindex, value in enumerate(row):
+			#Counting the number of living cells around each cell 
+			live_cells = current_state[rowindex-1,colindex-1]+current_state[rowindex-1,colindex]+current_state[rowindex-1,(colindex+1)%num_cols]+ \
+					  current_state[rowindex,colindex-1]+current_state[rowindex,(colindex+1)%num_cols]+current_state[(rowindex+1)%num_rows,colindex-1]+ \
+					  current_state[(rowindex+1)%num_rows,colindex]+current_state[(rowindex+1)%num_rows,(colindex+1)%num_cols]
+						
 			#Enforcing the Game of Life ruleset
 			if ((live_cells == 2 or live_cells == 3) and value == 1) or (live_cells == 3 and value == 0):
-				next_state[rowindex+1,colindex+1] = 1
+				next_state[rowindex,colindex] = 1
 			else:
-				next_state[rowindex+1,colindex+1] = 0
+				next_state[rowindex,colindex] = 0
 
 	return next_state.copy()
 
 #Function to draw the current state of all cells (white for living cells, black for dead cells)
 def draw_grid():
 	gameDisplay.fill(black)
-	for rowindex, row in enumerate(current_state[1:-1]):
+	for rowindex, row in enumerate(current_state):
 
-		for colindex, value in enumerate(row[1:-1]):
+		for colindex, value in enumerate(row):
 
 			if value == 1:
 				pygame.draw.rect(gameDisplay,white,[colindex*cell_size,rowindex*cell_size,cell_size,cell_size])
@@ -46,6 +48,8 @@ pygame.init()
 display_height = 720
 display_width = 1000
 cell_size = 10
+num_rows = display_height//cell_size
+num_cols = display_width//cell_size
 gridlines = False
 frame_rate = 120
 
@@ -55,9 +59,14 @@ black = (0,0,0)
 grey = (50,50,50)
 
 #Initializing the array of cells
-initial_state = np.zeros((int(display_height/cell_size)+2,int(display_width/cell_size)+2))
+initial_state = np.zeros((num_rows,num_cols))
 #Copy of initial state from which values will be read 
 current_state = initial_state
+
+for arg in sys.argv[1:]:
+	origin = tuple(map(int,arg.split("@")[1].split(",")))
+	pattern = arg.split("@")[0]
+	fill_cells((origin),patterns.patterns[pattern]["size"], patterns.patterns[pattern]["points"])
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption("Game of Life")
@@ -75,7 +84,7 @@ if __name__ == "__main__":
 				crashed = True
 
 			pos = pygame.mouse.get_pos()
-			coords = [int(pos[1]/cell_size)+1, int(pos[0]/cell_size)+1]
+			coords = [pos[1]//cell_size, pos[0]//cell_size]
 			pressed = pygame.mouse.get_pressed()
 
 			#Allows user to place or remove cells by holding down the mouse
